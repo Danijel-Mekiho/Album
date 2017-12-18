e2811fa0a95d81dbfeab294460ef6883c893ef79
blueror815 <blueror815@hotmail.com>
2017-12-18 14:44:21 +0100

    code clean

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
 import AlbumList from './src/components/AlbumList';
-// import App from './App';
+
 const App = () => (
     <View style={{flex: 1}}>
         <Header headerText={'Albums'}/>
